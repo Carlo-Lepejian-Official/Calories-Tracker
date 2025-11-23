c500fd78138ddb78dd252ffcbe70ffead288017b
Carlo Lepejian <carlo.lepejian@outlook.com>
2025-11-23 16:31:53 +1100

    Abstracts away a few components and make the edit and delete buttons for the calories entries

--- frontend/src/components/CalorieEntriesTable.jsx
+++ frontend/src/components/CalorieEntriesTable.jsx
@@ -9,10 +9,90 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Delete, Edit, Trash } from "lucide-react";
+import { Delete, Edit, Plus, Trash } from "lucide-react";
 import { Button } from "@/components/ui/Button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 
 const CalorieEntriesTable = () => {
+  const handleEdit = (e) => {};
+
+  const editButton = () => {
+    return (
+      <Dialog>
+        <DialogTrigger>
+          <Button size="sm" variant="ghost">
+            <Edit />
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Edit Entry</DialogTitle>
+          </DialogHeader>
+
+          <Input type="number" min={1}></Input>
+
+          <DialogFooter>
+            <DialogClose>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    );
+  };
+
+  const deleteButton = () => {
+    return (
+      <Dialog>
+        <DialogTrigger>
+          <Button size="sm" variant="ghost">
+            <Trash className="text-red-800" />
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you sure?</DialogTitle>
+          </DialogHeader>
+          <DialogFooter>
+            <DialogClose>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button variant="destructive" type="submit">
+              Yes, delete.
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    );
+  };
+
+  const calorieEntryRow = () => {
+    return (
+      <TableRow className="group">
+        <TableCell className="text-center">9:00 AM</TableCell>
+        <TableCell className="text-center">200</TableCell>
+        <TableCell
+          align="right"
+          className="opacity-0 group-hover:opacity-100 transition-opacity"
+        >
+          {editButton()}
+          {deleteButton()}
+        </TableCell>
+      </TableRow>
+    );
+  };
+
   return (
     <Table className="text-primary">
       <TableHeader>
@@ -22,23 +102,7 @@ const CalorieEntriesTable = () => {
           <TableHead className="w-1"></TableHead>
         </TableRow>
       </TableHeader>
-      <TableBody>
-        <TableRow className="group">
-          <TableCell className="text-center">9:00 AM</TableCell>
-          <TableCell className="text-center">200</TableCell>
-          <TableCell
-            align="right"
-            className="opacity-0 group-hover:opacity-100 transition-opacity"
-          >
-            <Button size="sm" variant="ghost">
-              <Edit />
-            </Button>
-            <Button size="sm" variant="ghost">
-              <Trash className="text-red-900" />
-            </Button>
-          </TableCell>
-        </TableRow>
-      </TableBody>
+      <TableBody>{calorieEntryRow()}</TableBody>
       <TableFooter>
         <TableRow>
           <TableCell className="text-center font-bold">Total</TableCell>
