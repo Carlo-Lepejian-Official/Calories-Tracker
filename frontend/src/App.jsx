cb7f1d69787fe2c938cebde4a22d521832bf9aad
Carlo Lepejian <carlo.lepejian@outlook.com>
2025-11-24 16:48:32 +1100

    Setup Clerk on the client side

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
 import Login from "./Pages/Login";
 import Dashboard from "./Pages/Dashboard";
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 function App() {
-  const [loggedIn, _setLoggedIn] = useState(true);
-
   return (
     <div className="w-full min-h-screen bg-background dark font-fredoka">
-      {loggedIn ? <Dashboard /> : <Login />}
+      <SignedOut>
+        <div className="w-full min-h-screen flex flex-col items-center justify-center">
+          <SignIn />
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Dashboard />
+      </SignedIn>
     </div>
   );
 }
