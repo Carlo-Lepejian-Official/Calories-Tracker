05d933bb56cf1e20347ec1b0fd0d995aeba089d0
Carlo Lepejian <carlo.lepejian@outlook.com>
2025-11-25 09:19:22 +1100

    Connect to the database when server starts

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,10 +5,19 @@ import {
   getAuth,
   requireAuth,
 } from "@clerk/express";
+import mongoose from "mongoose";
 import "dotenv/config";
 
 const app = express();
 const PORT = 3000;
+
+try {
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log("Connected to Database!");
+} catch (error) {
+  console.error("Error while connecting to database: ", error);
+  process.exit(1);
+}
 
 app.get("/api", requireAuth(), async (req, res) => {
   const { userId } = getAuth(req);
