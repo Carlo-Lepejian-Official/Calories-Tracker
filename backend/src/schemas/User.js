39c617a5f1e402be3005476b01c1a9d324aa85f2
Carlo Lepejian <carlo.lepejian@outlook.com>
2025-11-25 10:57:09 +1100

    Make the user id indexed

--- backend/src/schemas/User.js
+++ backend/src/schemas/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
+  userId: { type: String, required: true, indexed: true },
   DailyCalories: { type: Number, default: 2200 },
 });
 
