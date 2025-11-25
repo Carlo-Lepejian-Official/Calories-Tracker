aa756a037150e7014601f64d7a4bd4359f60aff9
Carlo Lepejian <carlo.lepejian@outlook.com>
2025-11-25 15:39:03 +1100

    Change the add entry button to a dialog instead of popover

--- frontend/src/components/CalorieEntry.jsx
+++ frontend/src/components/CalorieEntry.jsx
@@ -1,28 +1,47 @@
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 import { Button } from "@/components/ui/Button";
 import { Plus } from "lucide-react";
 import { Input } from "@/components/ui/input";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const CalorieEntry = ({ triggerClassName }) => {
   return (
-    <Popover>
-      <PopoverTrigger className={triggerClassName} asChild>
+    <Dialog>
+      <DialogTrigger className={triggerClassName} asChild>
         <Button className="w-full">
           <Plus />
           Add Entry
         </Button>
-      </PopoverTrigger>
-      <PopoverContent>
-        <div className="flex flex-col gap-2">
-          <Input type="number" min={1} className="text-center"></Input>
-          <Button>Add</Button>
-        </div>
-      </PopoverContent>
-    </Popover>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add Entry</DialogTitle>
+        </DialogHeader>
+
+        <Input
+          type="number"
+          min={1}
+          max={10000}
+          className="text-center"
+        ></Input>
+
+        <DialogFooter>
+          <DialogClose>
+            <Button variant="outline">Cancel</Button>
+          </DialogClose>
+          <DialogClose>
+            <Button type="submit">Add</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
 
