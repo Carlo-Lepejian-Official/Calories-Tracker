8dd1f9222e050a9e15cf42b9a99783df6e2d097b
Carlo Lepejian <carlo.lepejian@outlook.com>
2025-11-25 15:01:33 +1100

    Update the daily calories on the frontend based on the backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,12 +8,15 @@ import {
 import apiRoutes from "./middleware/apiRoutes.js";
 import ensureUserExists from "./middleware/ensureUserExists.js";
 import mongoose from "mongoose";
+import cors from "cors";
 import "dotenv/config";
 
 const app = express();
 const PORT = 3000;
 
 mongoose.connect(process.env.MONGODB_URI);
+
+app.use(cors());
 app.use("/api", requireAuth(), ensureUserExists, apiRoutes);
 
 app.listen(PORT, () => {
