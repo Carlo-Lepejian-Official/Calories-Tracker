7dc1075b7b28b98dd5d38e084d5fa8e3b8cab9ba
Carlo Lepejian <carlo.lepejian@outlook.com>
2025-11-25 11:13:00 +1100

    Create the calorieEntries route

--- backend/src/routes/calorieEntries.js
+++ backend/src/routes/calorieEntries.js
@@ -1,9 +1,16 @@
+import { clerkClient, getAuth } from "@clerk/express";
 import express from "express";
+import User from "../schemas/User.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  return res.send("Calories Entries: ");
+router.get("/", async (req, res) => {
+  // Get the user's UserId
+  const { userId } = getAuth(req);
+
+  // Find the user in the MongoDB database
+  const user = await User.findOne({ userId });
+  return res.send(user.dailyCalories);
 });
 
 export default router;
