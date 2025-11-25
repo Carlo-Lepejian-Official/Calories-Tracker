2e4e1af0c6b94bdaccb521b3d607776b25fda05e
Carlo Lepejian <carlo.lepejian@outlook.com>
2025-11-25 11:11:03 +1100

    Fix the case of the dailyCalories field

--- backend/src/schemas/User.js
+++ backend/src/schemas/User.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   userId: { type: String, required: true, indexed: true },
-  DailyCalories: { type: Number, default: 2200 },
+  dailyCalories: { type: Number, default: 2200 },
 });
 
 export default mongoose.model("User", userSchema, "Users");
