223ac4484faff1fc30bbc3c705515dfad958eb8d
Carlo Lepejian <carlo.lepejian@outlook.com>
2025-11-27 10:14:29 +1100

    Change paths in the server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,9 +6,16 @@ import admin from "firebase-admin";
 import { initializeApp } from "firebase-admin/app";
 import "dotenv/config";
 import validateUser from "./middleware/validateUser.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const distPath = path.join(__dirname, "../..", "frontend", "dist");
+
 initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_SERVICE_PROJECT_ID,
@@ -28,10 +35,10 @@ app.use(express.json());
 app.use("/api", validateUser, apiRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../../frontend/dist"));
+  app.use(express.static(distPath));
 
-  app.use("/*path", (req, res) => {
-    res.sendFile("../../frontend/dist/index.html");
+  app.use("*path", (req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
   });
 }
 
